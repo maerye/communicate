04d03923073e50ef343d94152fb958a02b4c1033
mazhiyuan <350046243@qq.com>
2019-04-22 11:10:39 +0800

    Initial commit

---
+++ server.go
@@ -0,0 +1,23 @@
+package main
+import(
+	. "communicate/lib"
+	"fmt"
+	"github.com/pkg/errors"
+)
+
+func server()error{
+	endpoint := NewEndPoint()
+
+	endpoint.AddHandleFunc("string", HandleStrings)
+	endpoint.AddHandleFunc("gob", HandleGob)
+
+	// 开始监听
+	return endpoint.Listen()
+}
+
+func main(){
+	err := server()
+	if err != nil {
+		fmt.Println("Error:", errors.WithStack(err))
+	}
+}
